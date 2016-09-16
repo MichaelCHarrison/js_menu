971cd0c5f33ba20d5afeb59b42cb2562348f3c05
Michael Harrison <michael.harrison2010@gmail.com>
2016-09-16 14:46:45 -0700

    Day after commit

--- js_menu/js_menu/js/JavaScript.js
+++ js_menu/js_menu/js/JavaScript.js
@@ -1,1 +1,9 @@
-﻿
+﻿function rollovermenu(showHide, menu) {
+    var linkObj = document.getElementById('mainLink');
+    var menuObj = document.getElementById(menu);
+    if (showHide == 0)
+    {
+        menuObj.style.display = 'none';
+        linkObj.setAttribute = 
+    }
+}
