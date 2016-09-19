0153696596573959847e3a3f1fb740e4aa82db16
Michael Harrison <michael.harrison2010@gmail.com>
2016-09-19 13:49:03 -0700

    Final Commit

--- js_menu/js_menu/js/JavaScript.js
+++ js_menu/js_menu/js/JavaScript.js
@@ -1,9 +1,15 @@
-﻿function rollovermenu(showHide, menu) {
+﻿function rolloverMenu(showHide, menu)
+{
     var linkObj = document.getElementById('mainLink');
     var menuObj = document.getElementById(menu);
-    if (showHide == 0)
-    {
+    if (showHide == 0) {
         menuObj.style.display = 'none';
-        linkObj.setAttribute = 
+        linkObj.setAttribute = ('class', 'mainNav');
+        linkObj.setAttribute = ('className', 'mainNav');
+    }
+    else {
+        menuObj.style.display = 'block';
+        linkObj.setAttribute = ('class', 'selectedRollover');
+        linkObj.setAttribute = ('className', 'selectedRollover');
     }
 }
